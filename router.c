e0f069081199ecb6abc8fe75fcee0dc8ba6b0294
Saumya Patil <saumyasachinpatil@gmail.com>
2023-07-27 16:40:40 +0530

    add request headers for html

--- router.c
+++ router.c
@@ -14,8 +14,13 @@ void router_handle_request(struct Request* req)
     sscanf(req->buffer, "%8s %256s %16s", method_buffer, path_buffer, http_version);
 
     sprintf(buffer, "HTTP/1.1 200 OK\r\n");
-    sprintf(buffer, "\r\n");
-    sprintf(buffer, "Hello at %s using %s", path_buffer, method_buffer);
+    sprintf(buffer + strlen(buffer), "Content-Type:text/html\r\n");
+    char body[4096];
+    sprintf(body, "<h1>Hello at %s using %s</h1>", path_buffer, method_buffer);
+    sprintf(buffer + strlen(buffer), "Content-Length:%ld\r\n", strlen(body));
+
+    sprintf(buffer  + strlen(buffer), "\r\n");
+    sprintf(buffer  + strlen(buffer), "%s", body);
 
     write(req->connfd, buffer, strlen(buffer));
 
